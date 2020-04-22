79d1e038bb0c312c8ab9fd8bf244b76a645dc0a0
tudor <tudor.rusu@hotmail.com>
2020-04-22 10:12:17 +0200

    style: fix some issues on title app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,9 +31,7 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <img alt="Guvernul Romaniei" class="top-brand" src="{{ asset('images/gov-ro.png') }}">
-                    <h1 class="top-brand-title">
-                        {!! config('app.name', 'DSP Declaraţii<br />Coronavirus COVID-19') !!}
-                    </h1>
+                    <h1 class="top-brand-title">DSP Declaraţii<br />Coronavirus COVID-19</h1>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
